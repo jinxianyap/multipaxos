d742d79b532e933e432ae170434998572c846286
Emily Haw <haw.emily@gmail.com>
2021-02-07 15:01:14 +0800

    add commander and edit acceptor to have config

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -2,23 +2,25 @@
 
 defmodule Acceptor do
 
-def start do
+def start(config) do
+    config = Configuration.node_id(config, "Acceptor", self())
+    Debug.starting(config)
     next -1 []
 end
 
 defp next pn accepted do
     receive do
-        {:prepare, sender, p_pn} -> 
+        {:prepare, leader, p_pn} -> 
             if p_pn > pn do
                 pn = p_pn
             end
-            send {sender, {:promise, self(), pn, accepted}} 
+            send leader, {:promise, self(), pn, accepted} 
             next pn accepted    
-        {:accept, sender, {p_pn, slot, cmd} = p_v} ->
+        {:accept, leader, {p_pn, slot, cmd} = p_v} ->
             if pn == p_pn do
                 accepted = accepted ++ [p_v]
             end
-            send {sender, {:accepted, self(), pn} }
+            send leader, {:accepted, self(), pn}
             next pn accepted
     end    
 end
---
+++ lib/commander.ex
@@ -0,0 +1,34 @@
+import 
+defmodule Commander do
+    def start(config, leader, acceptors, replicas, p_v) do
+        config = Configuration.node_id(config, "Commander", self())
+        Debug.starting(config)
+
+        for each <- acceptors do
+            send each {:accept, self(), p_v} 
+        end
+        waitfor = acceptors
+        next(leader, waitfor, acceptors, replicas, p_v)
+    end
+
+    defp next(leader, waitfor, acceptors, replicas, p_v) do
+        {pn, s, cmd} = p_v
+        receive do
+            {:accepted, acceptor, pn_accepted} -> 
+                if pn_accepted == pn do
+                    waitfor = List.delete(waitfor, acceptor)
+                    if length(waitfor) < length(acceptors) / 2 do
+                        for r <- replicas do
+                            send r {:decision, s, cmd}
+                        end
+                        Process.exit(self(), :normal)
+                    else
+                        next(leader, waitfor, acceptors, replicas, p_v)    
+                    end
+                else
+                    send leader {:preempted, pn_accepted}
+                    Process.exit(self(), :normal)    
+                end    
+        end
+    end
+end    
