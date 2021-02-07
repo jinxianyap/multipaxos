085715baa6075ba301f77831002ff397eb84a752
yapjinxian <yapjinxian@gmail.com>
2021-02-07 20:56:28 +0800

    completed replica

--- lib/replica.ex
+++ lib/replica.ex
@@ -3,12 +3,71 @@ defmodule Replica do
   def start(config, server_num, database) do
     config = Configuration.node_id(config, "Replica", server_num)
     Debug.starting(config)
-    receive do {:bind, leaders} -> next(leaders, database, 1, 1, [], [], []) end
+    receive do {:BIND, leaders} -> next(leaders, database, 1, 1, [], [], []) end
   end
 
-  defp next(leaders, state, slot_in, slot_out, requests, proposals, decisions) do
-    # receive do
-    #   {:request, }
-    # end
+  defp propose(leaders, slot_in, slot_out, requests, proposals, decisions) do
+    window = 3
+    if slot_in < slot_out + window and length(requests) > 0 do
+      c = Enum.at(requests, Util.random(length(requests)) - 1)
+      # reconfigs ignored for now
+      if Enum.find(decisions, fn {s_in, _c} -> s_in == slot_in end) == nil do
+        new_requests = List.delete(requests, c)
+        new_proposals = Util.list_union(proposals, {slot_in, c})
+        for each <- leaders do
+          send each, {:PROPOSE, slot_in, c}
+        end
+        propose(leaders, slot_in + 1, slot_out, new_requests, new_proposals, decisions)
+      else
+        propose(leaders, slot_in + 1, slot_out, requests, proposals, decisions)
+      end
+    else
+      {slot_in, requests, proposals}
+    end
+  end
+
+  defp perform(database, slot_out, decisions, _command = {client, cid, transactions}) do
+    # reconfigs ignored for now
+    if Enum.find(decisions, fn {s, _c} -> s < slot_out end) == nil do
+      send database, transactions
+      # how to get response from database?
+      send client, {:CLIENT_REPLY, cid, true}
+      slot_out + 1
+    else
+      slot_out + 1
+    end
+  end
+
+  defp allocate(database, slot_out, requests, proposals, decisions) do
+    decided = Enum.find(decisions, fn {s, _c} -> s == slot_out end)
+    if decided != nil do
+      {_, c_decided} = decided
+      proposed = Enum.find(proposals, fn {s, _c} -> s == slot_out end)
+      if proposed != nil do
+        {_, c_proposed} = proposed
+        new_proposals = List.delete(proposals, proposed)
+        new_requests = if c_decided != c_proposed do requests ++ [c_proposed] else requests end
+        new_slot_out = perform(database, slot_out, decisions, c_decided)
+        allocate(database, new_slot_out, new_requests, new_proposals, decisions)
+      else
+        new_slot_out = perform(database, slot_out, decisions, c_decided)
+        allocate(database, new_slot_out, requests, proposals, decisions)
+      end
+    else
+      {proposals, requests}
+    end
+  end
+
+  defp next(leaders, database, slot_in, slot_out, requests, proposals, decisions) do
+    receive do
+      {:CLIENT_REQUEST, c} ->
+        {new_slot_in, new_requests, new_proposals} = propose(leaders, slot_in, slot_out, requests++[c], proposals, decisions)
+        next(leaders, database, new_slot_in, slot_out, new_requests, new_proposals, decisions)
+      {:DECISION, s, c} ->
+        new_decisions = decisions ++ [{s, c}]
+        {new_proposals, new_requests} = allocate(database, slot_out, requests, proposals, new_decisions)
+        {new_slot_in, new_requests_, new_proposals_} = propose(leaders, slot_in, slot_out, new_requests, new_proposals, new_decisions)
+        next(leaders, database, new_slot_in, slot_out, new_requests_, new_proposals_, new_decisions)
+    end
   end
 end
--- lib/scout.ex
+++ lib/scout.ex
@@ -4,26 +4,26 @@ defmodule Scout do
     config = Configuration.node_id(config, "Scout", pn)
     Debug.starting(config)
     for each <- acceptors do
-      send each, {:prepare, self(), pn}
+      send each, {:PREPARE, self(), pn}
     end
     next(leader, acceptors, pn, acceptors, [])
   end
 
   defp next(leader, acceptors, pn, waitfor, p_values) do
     receive do
-      {:promise, a_id, pn_returned, p_accepted} ->
-        if pn == pn_returned do
-          new_p_values = p_values ++ p_accepted
+      {:PROMISE, a_id, pn_returned, p_accepted} ->
+        if Util.compare_pn(pn, pn_returned) == 0 do
+          new_p_values = Util.list_union(p_values,p_accepted)
           new_waitfor = List.delete(waitfor, a_id)
           if length(new_waitfor) < length(acceptors) / 2 do
-            send leader, {:adopted, pn, new_p_values}
-            Process.exit(self(), :normal)
+            send leader, {:ADOPTED, pn, new_p_values}
+            Process.exit(self(), :kill)
           else
             next(leader, acceptors, pn, new_p_values, new_waitfor)
           end
         else
-          send leader, {:preempted, pn_returned}
-          Process.exit(self(), :normal)
+          send leader, {:PREEMPTED, pn_returned}
+          Process.exit(self(), :kill)
         end
     end
   end
