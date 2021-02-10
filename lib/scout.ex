60846588b7b2d722c8495d2cc4f5c3d9bcf825f9
Emily Haw <haw.emily@gmail.com>
2021-02-11 00:07:15 +0800

    fix bug in scout that was adding item to list instead of list to list, line 18

--- lib/scout.ex
+++ lib/scout.ex
@@ -13,13 +13,16 @@ defmodule Scout do
     receive do
       {:PROMISE, a_id, pn_returned, p_accepted} ->
         if Util.compare_pn(pn, pn_returned) == 0 do
-          new_p_values = Util.list_union(p_values,p_accepted)
+          # IO.puts("p_values: #{inspect p_values}")
+          # IO.puts("p_accepted: #{inspect p_accepted}") 
+          new_p_values = List.flatten(Enum.map(p_accepted, fn accepted -> Util.list_union(p_values,accepted) end))
+          # IO.puts "PROMISE new_p_values: #{inspect new_p_values}"
           new_waitfor = List.delete(waitfor, a_id)
           if length(new_waitfor) < length(acceptors) / 2 do
             send leader, {:ADOPTED, pn, new_p_values}
             Process.exit(self(), :kill)
           else
-            next(leader, acceptors, pn, new_p_values, new_waitfor)
+            next(leader, acceptors, pn, new_waitfor, new_p_values)
           end
         else
           send leader, {:PREEMPTED, pn_returned}
