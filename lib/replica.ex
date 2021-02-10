10d8885f528e6bd6701a34ed645eeb4e1a995717
yapjinxian <yapjinxian@gmail.com>
2021-02-10 18:38:58 +0800

    included reconfig handling in replica

--- lib/replica.ex
+++ lib/replica.ex
@@ -6,30 +6,47 @@ defmodule Replica do
     receive do {:BIND, leaders} -> next(leaders, database, 1, 1, [], [], []) end
   end
 
+  # assuming a reconfig request is in the form { :CLIENT_REQUEST, {cid, sent_index, {:RECONFIG, new_leaders}} }
+  defp isreconfig(_command = {name, _leaders}) do
+    name == :RECONFIG
+  end
+
   defp propose(leaders, slot_in, slot_out, requests, proposals, decisions) do
     window = 3
     if slot_in < slot_out + window and length(requests) > 0 do
       c = Enum.at(requests, Util.random(length(requests)) - 1)
-      # reconfigs ignored for now
+
+      new_leaders = fn ->
+        Enum.find(decisions, fn {s_in, c} ->
+          # c is of the form {cid, sent_index, {:RECONFIG, new_leaders}}
+          {_, _, op} = c
+          if s_in == slot_in - window and isreconfig(op) do
+            {_, ls} = op
+            ls
+          else
+            leaders
+          end
+        end)
+      end
+
       if Enum.find(decisions, fn {s_in, _c} -> s_in == slot_in end) == nil do
         new_requests = List.delete(requests, c)
         new_proposals = Util.list_union(proposals, {slot_in, c})
-        for each <- leaders do
+        for each <- new_leaders do
           send each, {:PROPOSE, slot_in, c}
         end
-        propose(leaders, slot_in + 1, slot_out, new_requests, new_proposals, decisions)
+        propose(new_leaders, slot_in + 1, slot_out, new_requests, new_proposals, decisions)
       else
-        propose(leaders, slot_in + 1, slot_out, requests, proposals, decisions)
+        propose(new_leaders, slot_in + 1, slot_out, requests, proposals, decisions)
       end
     else
-      {slot_in, requests, proposals}
+      {leaders, slot_in, requests, proposals}
     end
   end
 
-  defp perform(database, slot_out, decisions, _command = {client, cid, transactions}) do
-    # reconfigs ignored for now
-    if Enum.find(decisions, fn {s, _c} -> s < slot_out end) == nil do
-      send database, transactions
+  defp perform(database, slot_out, decisions, _request = {client, cid, op}) do
+    if Enum.find(decisions, fn {s, _c} -> s < slot_out end) == nil and !isreconfig(op) do
+      send database, op
       # how to get response from database?
       send client, {:CLIENT_REPLY, cid, true}
       slot_out + 1
@@ -61,13 +78,13 @@ defmodule Replica do
   defp next(leaders, database, slot_in, slot_out, requests, proposals, decisions) do
     receive do
       {:CLIENT_REQUEST, c} ->
-        {new_slot_in, new_requests, new_proposals} = propose(leaders, slot_in, slot_out, requests++[c], proposals, decisions)
-        next(leaders, database, new_slot_in, slot_out, new_requests, new_proposals, decisions)
+        {new_leaders, new_slot_in, new_requests, new_proposals} = propose(leaders, slot_in, slot_out, requests++[c], proposals, decisions)
+        next(new_leaders, database, new_slot_in, slot_out, new_requests, new_proposals, decisions)
       {:DECISION, s, c} ->
         new_decisions = decisions ++ [{s, c}]
         {new_proposals, new_requests} = allocate(database, slot_out, requests, proposals, new_decisions)
-        {new_slot_in, new_requests_, new_proposals_} = propose(leaders, slot_in, slot_out, new_requests, new_proposals, new_decisions)
-        next(leaders, database, new_slot_in, slot_out, new_requests_, new_proposals_, new_decisions)
+        {new_leaders, new_slot_in, new_requests_, new_proposals_} = propose(leaders, slot_in, slot_out, new_requests, new_proposals, new_decisions)
+        next(new_leaders, database, new_slot_in, slot_out, new_requests_, new_proposals_, new_decisions)
     end
   end
 end
